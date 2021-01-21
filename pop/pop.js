51750be785db4d0bf9f51c4519df78d2b3193b2f
ChloeArd <chloe.ardoise@gmail.com>
2021-01-21 16:31:09 +0100

    Playlist Musical

--- pop/pop.js
+++ pop/pop.js
@@ -1,8 +1,10 @@
 let songPop = [
-    {"title" : "7 rings", "artist" : "Ariana Grande", "link" : "https://www.youtube.com/watch?v=QYh6mYIJG2Y"}
+    {"title" : "7 rings", "artist" : "Ariana Grande", "link" : "https://www.youtube.com/watch?v=QYh6mYIJG2Y"},
+    {"title" : "On verra bien", "artist" : "Amir", "link" : "https://www.youtube.com/watch?v=XRi1lhm6qAQ"},
 ]
-
+let containerVideos = $("#video");
 let containerSongs = $("#containerSongs").first();
+let nbClick = 0;
 containerSongs.append(
     "<div class=\"ligne\"></div>" +
     "<div class='songs'>" +
@@ -14,3 +16,13 @@ containerSongs.append(
 );
 
 $("#link1").attr("href", songPop[0].link);
+$("#play1").click(function (){
+    if(nbClick < 1){
+        containerVideos.append("<iframe id='ariana' width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/QYh6mYIJG2Y\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>\n");
+        nbClick++;
+    }
+    else {
+        $("#ariana").css("display", "none");
+        nbClick = 0;
+    }
+})

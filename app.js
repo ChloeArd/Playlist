4bfbe46a155ef376f6735961fdf311fe24f63614
ChloeArd <chloe.ardoise@gmail.com>
2021-01-22 12:01:53 +0100

    Playlist Musical

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 let containerMusics = $("#musics");
+let index = 0;
 
 $("#createPlaylist").click(function () {
     let namePlaylist = prompt("Nom de t'as playlist ?");
     let urlImage = prompt("Insère un lien d'une image");
-
+    index++;
     containerMusics.append(
-        "<div class='playlists' href=''>" +
+        "<div class='playlists'>" +
     "<img class='imageProj' src='"+ urlImage +"'>" +
-        "<button class='playPlaylist'>" + namePlaylist + "</button></div>"
+        "<button class='playPlaylist' id='playlist" + index + "'>" + namePlaylist + "</button></div>"
     );
 })

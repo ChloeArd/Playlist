a5d05be13ad5f0285c4b59255c9316f06168bb1d
ChloeArd <chloe.ardoise@gmail.com>
2021-01-24 17:02:43 +0100

    Playlist Musical

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let containerMusics = $("#musics");
+let containerSongs = $("#containerSongs").first()
+let containerVideos = $("#video");
 let index = 0;
+let nbClick = 0;
 
 $("#createPlaylist").click(function () {
     let namePlaylist = prompt("Nom de t'as playlist ?");
@@ -7,7 +10,29 @@ $("#createPlaylist").click(function () {
     index++;
     containerMusics.append(
         "<div class='playlists'>" +
-    "<img class='imageProj' src='"+ urlImage +"'>" +
-        "<button class='playPlaylist' id='playlist" + index + "'>" + namePlaylist + "</button></div>"
+            "<img class='imageProj' src='"+ urlImage +"'>" +
+            "<button class='playPlaylist' id='playlist" + index + "'>" + namePlaylist + "</button></div>"
     );
-})
+    $("#playlist" + index).click(function () {
+        let title = prompt("Titre de t'as musique ?");
+        let artist = prompt("Nom de ton artiste ?");
+        let urlLink = prompt("Lien YouTube de t'as musique");
+
+        containerSongs.append(
+            "<div class=\"ligne\"></div>" +
+            "<div class='songs'>" +
+            "<p class='title'>" + title + "</p>" +
+            "<p class='artist'>" + artist + "</p>" +
+            "<a class='link' id='link" + index +"' href=''> Clique </a>" +
+            "<button class='play' id='play" + index + "'> Play </button>" +
+            "</div>"
+        );
+        $("#link" + index).attr("href", urlLink);
+
+        $("#play" + index).click(function (){
+            let videoLink = prompt("Copie le code d'intégration de la video (clique droit sur la video) sur YouTube '<iframe></iframe>'  ?");
+            containerVideos.append(videoLink);
+        })
+    });
+});
+
